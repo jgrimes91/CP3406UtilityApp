e5d020195af204cf86dfdc2014c0299a11d167d5
jgrimes91 <jessica.grimes@my.jcu.edu.au>
2023-03-29 22:04:24 +1000

    Update MainActivity.kt

--- EyeCare/app/src/main/java/au/edu/jcu/utilityapp/eyecare/MainActivity.kt
+++ EyeCare/app/src/main/java/au/edu/jcu/utilityapp/eyecare/MainActivity.kt
@@ -4,9 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var textView: TextView
 
     private val launcher =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
@@ -18,6 +21,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.timer_tv)
 
         val settingsButton: Button = findViewById(R.id.settings_btn)
         settingsButton.setOnClickListener {
