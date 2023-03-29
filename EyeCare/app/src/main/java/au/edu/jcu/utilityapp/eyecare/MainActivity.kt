e0a52ab9990517ec830cf12de44850fc3d108728
jgrimes91 <jessica.grimes@my.jcu.edu.au>
2023-03-29 22:00:46 +1000

    Setting Intent

--- EyeCare/app/src/main/java/au/edu/jcu/utilityapp/eyecare/MainActivity.kt
+++ EyeCare/app/src/main/java/au/edu/jcu/utilityapp/eyecare/MainActivity.kt
@@ -1,11 +1,28 @@
 package au.edu.jcu.utilityapp.eyecare
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
+
+    private val launcher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK) {
+                println(it.data?.getStringExtra("value"))
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val settingsButton: Button = findViewById(R.id.settings_btn)
+        settingsButton.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            launcher.launch(intent)
+        }
     }
 }
--- EyeCare/app/src/main/java/au/edu/jcu/utilityapp/eyecare/SettingsActivity.kt
+++ EyeCare/app/src/main/java/au/edu/jcu/utilityapp/eyecare/SettingsActivity.kt
@@ -1,11 +1,27 @@
 package au.edu.jcu.utilityapp.eyecare
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
+
+    private val launcher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK) {
+                println(it.data?.getStringExtra("value"))
+            }
+        }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val saveButton:Button = findViewById(R.id.save_btn)
+        saveButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            launcher.launch(intent)
+        }
     }
 }
