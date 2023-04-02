a4b949645f458d050495847086ae1d573cb61fb0
jgrimes91 <jessica.grimes@my.jcu.edu.au>
2023-04-03 03:27:48 +1000

    Timer functionality

--- EyeCare/app/src/main/java/au/edu/jcu/utilityapp/eyecare/MainActivity.kt
+++ EyeCare/app/src/main/java/au/edu/jcu/utilityapp/eyecare/MainActivity.kt
@@ -4,27 +4,64 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import au.edu.jcu.utilityapp.eyecare.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var eyeTimer: CountDownTimer
 
-    lateinit var binding: ActivityMainBinding
 
-    var isRunning: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.startBtn.setOnClickListener {  }
-        binding.cancelBtn.setOnClickListener { }
-        binding.resetBtn.setOnClickListener {  }
+
+
+        binding.startBtn.setOnClickListener { startTimer() }
+        binding.stopBtn.setOnClickListener { stopTimer() }
         binding.settingsBtn.setOnClickListener { changeSettings() }
+
 
     }
 
 
+    private fun stopTimer() {
+        val blankTime = "00:00"
+        binding.timerTv.text = blankTime
+        eyeTimer.cancel()
+    }
+
+    private fun startTimer() {
+        intervalTimer()
+    }
+
+    private fun intervalTimer() {
+        val timerLength: Long = 20 * 60_000
+        eyeTimer = object : CountDownTimer(timerLength, 1000) {
+            override fun onTick(msToFinised: Long) {
+                val minutes = msToFinised / 1000 / 60
+                val seconds = msToFinised / 1000 % 60
+                displayTimer(minutes, seconds)
+            }
+
+            override fun onFinish() {
+                eyeTimer.cancel()
+                // Add a snackbar?
+            }
+        }
+        eyeTimer.start()
+    }
+
+
+    private fun displayTimer(minutes: Long, seconds: Long) {
+        val formattedTime = "%02d:%02d".format(minutes, seconds)
+        binding.timerTv.text = formattedTime
+
+    }
 
     /**
      * Move to SettingsActivity to change settings.
--- EyeCare/app/src/main/java/au/edu/jcu/utilityapp/eyecare/SettingsActivity.kt
+++ EyeCare/app/src/main/java/au/edu/jcu/utilityapp/eyecare/SettingsActivity.kt
@@ -1,63 +1,53 @@
 package au.edu.jcu.utilityapp.eyecare
 
-import android.content.Context.INPUT_METHOD_SERVICE
 import android.content.Intent
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat.getSystemService
-import androidx.core.content.ContextCompat.startActivity
-import androidx.databinding.BindingAdapter
 import au.edu.jcu.utilityapp.eyecare.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
-
     lateinit var binding: ActivitySettingsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
         binding.saveBtn.setOnClickListener { saveSettings() }
 
 
-        binding.intervalInput.setOnKeyListener { view, keyCode, _ ->
-            handleKeyEvent(
-                view,
-                keyCode
-            )
-        }
-    }
+        /**
+         * Return back to MainActivity screen
+         */
 
-    /**
-     * Return back to MainActivity screen
-     */
+
+        /**
+         * Helper method, if enter key is pressed then close the on-screen keyboard
+         *
+         * @param view View
+         * @param keyCode Keycode
+         */
+
+        fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+            if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                // Hide the keyboard
+                val inputMethodManager =
+                    getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+                return true
+            }
+            return false
+        }
+
+    }
 
     fun saveSettings() {
-
-
-        val intent = Intent(this, SettingsActivity::class.java)
+        val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
-
-    /**
-     * Helper method, if enter key is pressed then close the on-screen keyborad
-     *
-     * @param view View
-     * @param keyCode Keycode
-     */
-
-    fun handleKeyEvent(view: View, keyCode: Int): Boolean {
-        if (keyCode == KeyEvent.KEYCODE_ENTER) {
-            // Hide the keyboard
-            val inputMethodManager =
-                getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
-            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
-            return true
-        }
-        return false
-    }
-
 }
